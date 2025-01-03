0cde609c85e1b037eb4a0aaf98749799b7c262e4
Murray Bealby <murraybealby@c02fp2mxq6lr.lan>
2025-01-03 21:39:30 +0100

    Mob nav functionality plus styling - WIP

---
+++ static/js/mobile-nav.js
@@ -0,0 +1,12 @@
+// FULL SCREEN OVERLAY FOR MOBILE
+
+// Open navbar when toggle clicked on mobile
+function openMenu(){
+    document.getElementById("navbar").style.height = "100%";
+  }
+  
+// Close menu for mobile
+function closeMenu(){
+document.getElementById("navbar").style.height = "0%";
+   }
+  
