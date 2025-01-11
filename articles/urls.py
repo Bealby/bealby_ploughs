02ce5069a2f874006f3dce2fbd3c6f0edc25c083
Murray Bealby <murraybealby@c02fp2mxq6lr.lan>
2025-01-11 05:06:39 +0100

    Add paths/URLS/ views for multiple links for article app

--- articles/urls.py
+++ articles/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.articles, name='articles')
+    path('articles_1', views.articles_1, name='articles_1'),
+    path('articles_2', views.articles_2, name='articles_2'),
+    path('articles_3', views.articles_3, name='articles_3'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,7 +2,20 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def articles(request):
+def articles_1(request):
     '''A view to return the index page'''
     
-    return render(request, 'articles/articles.html')
+    return render(request, 'articles/articles_1.html')
+
+
+def articles_2(request):
+    '''A view to return the index page'''
+    
+    return render(request, 'articles/articles_2.html')
+
+
+
+def articles_3(request):
+    '''A view to return the index page'''
+    
+    return render(request, 'articles/articles_3.html')
