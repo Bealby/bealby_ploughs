928d8882c06ee88ab487473a870b5f76ce695b16
Murray Bealby <murraybealby@c02fp2mxq6lr.lan>
2025-01-18 13:42:13 +0100

    Add research model. Layout html

--- research/models.py
+++ research/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+'''Pillow must be installed'''
 
-# Create your models here.
+
+class Research_1(models.Model):
+    '''Programmatic Name'''
+
+    class Meta:
+        verbose_name_plural = 'Invoice Journals'
+
+    image_text = models.TextField("Text", max_length=1000, null=True, blank=True)
+    image = models.ImageField("Image", blank=True, null=True, help_text="Please upload images 600x400. Max 300px")
+    image_title = models.CharField("1. Image Title", max_length=254, null=True, blank=True)
+    image_description = models.CharField("1. Image Description", max_length=254, null=True, blank=True)
+    image_2 = models.ImageField("2. Image", blank=True, null=True, help_text="Please upload images 600x400. Max 300px")
+    image_title_2 = models.CharField("2. Image Title", max_length=254, null=True, blank=True)
+    image_description_2 = models.CharField("2. Image Description", max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.image_text
