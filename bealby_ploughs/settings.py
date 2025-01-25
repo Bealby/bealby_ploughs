5b24ebeee08b5ee7882a2e0aaec91d1d60787a0e
Bealby <mgbealby@gmail.com>
2025-01-25 17:07:21 +0100

    Styling about page

--- bealby_ploughs/settings.py
+++ bealby_ploughs/settings.py
@@ -58,7 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
+     #'allauth.account.middleware.AccountMiddleware',
+    'django_auto_logout.middleware.auto_logout',
 ]
 
 ROOT_URLCONF = 'bealby_ploughs.urls'
