e917dfffaff1a0ac70f97a33325d5cfd2749b4a5
Bealby <mgbealby@gmail.com>
2025-01-26 14:46:45 +0100

    Install wheel no dependencies

--- bealby_ploughs/__init__.py
+++
@@ -1,6 +0,0 @@
-# my_package/__init__.py
-__version__ = '1.0.0'
-
-def get_version():
-    from bealby_ploughs import __version__  # Ensure this import works
-    return __version__
--- build/lib/bealby_ploughs/__init__.py
+++
@@ -1,6 +0,0 @@
-# my_package/__init__.py
-__version__ = '0.1.0'
-
-def get_version():
-    from bealby_ploughs import __version__  # Ensure this import works
-    return __version__
