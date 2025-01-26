1c1e671aebeb126266efe43b1a77e67af1c62c13
Bealby <mgbealby@gmail.com>
2025-01-26 13:40:01 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bealby_ploughs',  # Replace with your package name
+    version='0.1.0',  # Initial version
+    author='Murray Bealby',  # Your name
+    author_email='bealbyploughs@gmail.com',  # Your email
+    description='Bealby Ploughs',  # Short description
+    long_description=open('README.md').read(),  # Read long description from README
+    long_description_content_type='text/markdown',  # Format of the long description
+    url='https://github.com/bealby/bealby_ploughs',  # URL to your project
+    packages=find_packages(),  # Automatically find packages in the directory
+    install_requires=[  # List of dependencies
+        'numpy',  # Example dependency
+    ],
+    classifiers=[  # Classifiers for the package
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',  # Minimum Python version required
+)
