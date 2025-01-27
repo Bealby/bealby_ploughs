55675ec7b5f8f08b87408468fd20933387081f2c
Bealby <mgbealby@gmail.com>
2025-01-27 22:45:24 +0100

    Deploy

--- bealby_ploughs/settings.py
+++ bealby_ploughs/settings.py
@@ -61,7 +61,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
+    'allauth.account.middleware.AuthenticationMiddleware',
 ]
 
 ROOT_URLCONF = 'bealby_ploughs.urls'
