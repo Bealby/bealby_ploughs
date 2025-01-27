2c6051aa383af18a1744fec7e1e6314ce073bea0
Bealby <mgbealby@gmail.com>
2025-01-28 00:40:28 +0100

    Deploy

--- bealby_ploughs/settings.py
+++ bealby_ploughs/settings.py
@@ -17,6 +17,16 @@ if os.path.exists('env.py'):
 
 from pathlib import Path
 
+import os
+
+# Set static root for Heroku
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+STATIC_URL = '/static/'
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -181,7 +191,7 @@ if 'USE_AWS' in os.environ:
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
     STATICFILES_LOCATION = 'static'
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
