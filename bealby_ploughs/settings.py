199d246a1af41d6c8a4ec46316400581a6ecd403
Bealby <mgbealby@gmail.com>
2025-01-28 10:25:19 +0100

    Deploy

--- bealby_ploughs/settings.py
+++ bealby_ploughs/settings.py
@@ -157,11 +157,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript,  Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'  
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
