bb191bab72575759cf3602b5e9f9de36accf990b
Bealby <mgbealby@gmail.com>
2025-04-06 22:19:59 +0200

    Change Bealby family model

--- research/models.py
+++ research/models.py
@@ -45,13 +45,13 @@ class BealbyFamily(models.Model):
         verbose_name_plural = 'Bealby Family'
 
     main_header = models.CharField("Main Title", max_length=254, null=True, blank=True)
-    authur = models.TextField("Arthur", max_length=1000, null=True, blank=True)
+    authur = models.CharField("Arthur", max_length=254, null=True, blank=True)
     sub_header_1 = models.CharField("Sub Header 1", max_length=254, null=True, blank=True)
-    paragraph_1 = models.TextField("Text_1", max_length=1000, null=True, blank=True)
+    paragraph_1 = models.TextField("Text_1", max_length=3000, null=True, blank=True)
     sub_header_2 = models.CharField("Sub Header 1", max_length=254, null=True, blank=True)
-    paragraph_2 = models.TextField("Text_2", max_length=1000, null=True, blank=True)
+    paragraph_2 = models.TextField("Text_2", max_length=3000, null=True, blank=True)
     sub_header_3 = models.CharField("Sub Header 1", max_length=254, null=True, blank=True)
-    paragraph_3 = models.TextField("Text_3", max_length=1000, null=True, blank=True)
+    paragraph_3 = models.TextField("Text_3", max_length=3000, null=True, blank=True)
 
     def __str__(self):
         return self.text
