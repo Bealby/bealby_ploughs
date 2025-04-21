ff4aca3de39a6d124a0ae198dc9c7036a6a73219
Bealby <mgbealby@gmail.com>
2025-04-21 20:23:09 +0200

    Add sitemapo.xml

--- bealby_ploughs/urls.py
+++ bealby_ploughs/urls.py
@@ -17,6 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,6 +28,10 @@ urlpatterns = [
     path('research/', include('research.urls')),
     path('gallery/', include('gallery.urls')),
     path('contact/', include('contact.urls')),
+    re_path(r'^sitemap\.xml$', serve, {
+        'document_root': settings.STATIC_ROOT,
+        'path': 'sitemap.xml'
+    }),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # default: "Django Administration"
