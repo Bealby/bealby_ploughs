22fab230c598aa956e5b729e8ec88beea4a86540
Bealby <mgbealby@gmail.com>
2025-04-23 21:39:44 +0200

    Remove sitemap

--- bealby_ploughs/urls.py
+++ bealby_ploughs/urls.py
@@ -17,12 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.sitemaps.views import sitemap
-from bealby_ploughs.sitemaps import StaticViewSitemap
-
-sitemaps = {
-    'static': StaticViewSitemap,
-}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
