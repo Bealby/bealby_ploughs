6108580c75668fdb0543b910753f36cf4821ca49
Bealby <mgbealby@gmail.com>
2025-04-26 06:03:34 +0200

    Set up sitemaps

--- home/sitemaps.py
+++ home/sitemaps.py
@@ -1,13 +1,17 @@
 from django.contrib.sitemaps import Sitemap
-from django.shortcuts import reverse
+from django.urls import reverse
+from django.conf import settings
 
 class StaticViewSitemap(Sitemap):
     priority = 0.5
     changefreq = 'monthly'
 
     def items(self):
-        return ['home', 'the_thoroton_society', 'workshop_anecdote', 'family_recollection', 'invoice_journal', 'brook_house_A', 'bealby_family_A', 'bealby_family', 'brook_house', 'bealby_workshop', 'bealby_plough', 'bealby_forest_cart', 'workshop_tool']  # these are your URL names
+        return ['home', 'the_thoroton_society', 'workshop_anecdote', 'family_recollection', 
+                'invoice_journal', 'brook_house_A', 'bealby_family_A', 'bealby_family', 
+                'brook_house', 'bealby_workshop', 'bealby_plough', 'bealby_forest_cart', 
+                'workshop_tool']  # These are your URL names
 
     def location(self, item):
-        return reverse(item)
-
+        # Prepend the base URL (SITE_URL) to the relative URL
+        return settings.SITE_URL + reverse(item)
