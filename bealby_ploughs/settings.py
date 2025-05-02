8c78fd77f6fecf862ec7bba655959a305a24e8f9
Bealby <mgbealby@gmail.com>
2025-05-02 21:55:16 +0200

    Styling index.html

--- bealby_ploughs/settings.py
+++ bealby_ploughs/settings.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'allauth.account.middleware.AccountMiddleware',
+    # 'allauth.account.middleware.AccountMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
